b160984fe2525b0012c6421ccb15e136dcd0a22f
Hariprasath Muthumanickam <39271101+mkhariprasath@users.noreply.github.com>
2022-03-08 17:24:51 +0530

    Update test-tdd.cpp

--- test-tdd.cpp
+++ test-tdd.cpp
@@ -1,3 +1,11 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
 #include "test/catch.hpp"
+
+
+TEST_CASE("") {
+
+  int chargingSession[] = {4,5}
+  REQUIRE(getRangeReading(chargingSession) == "4-5, 2");
+
+}
