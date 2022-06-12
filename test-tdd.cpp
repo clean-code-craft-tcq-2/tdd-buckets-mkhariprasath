1ce6ac605b283af14e2e059920729a769ab46794
Hariprasath Muthumanickam <39271101+mkhariprasath@users.noreply.github.com>
2022-06-12 11:06:35 +0530

    Update test-tdd.cpp

--- test-tdd.cpp
+++ test-tdd.cpp
@@ -1,11 +1,24 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "frequentCurrentRanges.h"
+#include "adc.h"
+#include <assert.h>
 
-#include "test/catch.hpp"
+#define A2D_RESOLUTION  12
+#define MAXCURRENTVALUE 10
+#define MINCURRENTVALUE 0
+
+/*infers the number of ranges when all enteries are same*/
+void TEST_CASE_01()
+{
+    float readingsArray[]={4,4,4,4};
+    int numOfReadings = sizeof(readingsArray)/sizeof(readingsArray[0]);
+    printf ("\n TEST_CASE_01 \nInput Array : ");
+    printArray (readingsArray, numOfReadings);
+    assert(detectRanges(readingsArray, numOfReadings) == 1);
+}
 
 
-TEST_CASE("") {
-
-  int chargingSession[] = {4,5}
-  REQUIRE(getRangeReading(chargingSession) == "4-5, 2");
-
+int main()
+{
+	TEST_CASE_01();
+	return 0;
 }
